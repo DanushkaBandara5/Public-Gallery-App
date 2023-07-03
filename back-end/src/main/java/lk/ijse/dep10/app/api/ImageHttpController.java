eabc460dd48c55be0fb3f1b23991b9c84b8d898d
danushka kasun <danushkakasun05@gmail.com>
2023-07-03 22:22:48 +0530

    completin of getAllImages service

--- back-end/src/main/java/lk/ijse/dep10/app/api/ImageHttpController.java
+++ back-end/src/main/java/lk/ijse/dep10/app/api/ImageHttpController.java
@@ -1,17 +1,41 @@
 package lk.ijse.dep10.app.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.servlet.ServletContext;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/api/v1/images")
 public class ImageHttpController {
+    @Autowired
+    private ServletContext servletContext;
     @GetMapping
-    public String getMapping(){
-        return "this is a image gallery";
+    public List<String> getMapping(UriComponentsBuilder uriBuilder){
+        List<String> imageUrlList =new ArrayList<>();
+        String imgDirPath = servletContext.getRealPath("/images");
+        System.out.println(imgDirPath);
+        File ImageDir = new File(imgDirPath);
+        String[] imageFiles = ImageDir.list();
+        System.out.println(imageFiles);
+        for (String imageFileName : imageFiles) {
+            UriComponentsBuilder uriComponentsBuilder = uriBuilder.cloneBuilder();
+            String url =uriComponentsBuilder.pathSegment("images",imageFileName).toUriString();
+            imageUrlList.add(url);
+        }
+        return  imageUrlList;
+
+
     }
 }
